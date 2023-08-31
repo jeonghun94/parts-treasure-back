79a1b6f6fc3ea7bc1c61b9fbdff74a9402fbbe38
조정훈 <jeongh1021@naver.com>
2023-08-31 22:53:31 +0900

    parts-treasure-back 프로젝트 생성 🚀

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserService } from './user/user.service';
+import { PrismaService } from './prisma/prisma.service';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [AppController],
+  providers: [AppService, UserService, PrismaService],
+})
+export class AppModule {}
